31d7374f67e77442fb34e7da2296980d23699bed
Leann Kim <leannminakim@gmail.com>
2018-05-15 02:10:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma (string)
   if string != string.upcase
     "HUH?! SPEAK UP, SONNY!"
-  elsif string == string.upcase
+  elsif string == string.upcase && string != "I LOVE YOU GRANDMA!"
     "NO, NOT SINCE 1938!"
   elsif string == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN"
